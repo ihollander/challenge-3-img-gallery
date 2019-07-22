723f54ce35e032f6e04aee8f50ca0eb68c2bbc9f
ihollander <ianholla@gmail.com>
2019-07-22 07:58:33 -0400

    initial functionality done

---
+++ scripts/api.js
@@ -0,0 +1,27 @@
+export default (() => {
+  const key =
+    "cdda9bdc98c0d5aac29b0d986d84703a26cedaef638657cb3ee6667dd94c0758";
+  const baseUrl = "https://api.unsplash.com";
+  const defaultHeaders = {
+    Authorization: `Client-ID ${key}`
+  };
+
+  const fetchAndParse = async (url, options) => {
+    const response = await fetch(url, options);
+    if (!response.ok) throw response;
+
+    return response.json();
+  };
+
+  const getImages = async (query, page = 1, per_page = 25) => {
+    const url = `${baseUrl}/search/photos?page=${page}&query=${query}&per_page=${per_page}&orientation=landscape`;
+    return fetchAndParse(url, {
+      method: "GET",
+      headers: defaultHeaders
+    });
+  };
+
+  return {
+    getImages
+  };
+})();
--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,62 @@
-console.log('hello world!');
+import adapter from "./api.js";
+import { arrayToGrid } from "./helpers.js";
 
+// DOM elements
+const searchForm = document.querySelector("#image-search");
+const photoContainer = document.querySelector("#photo-container");
+const imageTemplate = document.querySelector("#image-column");
+
+// handlers
+const handleSearchSubmit = e => {
+  e.preventDefault();
+  const search = e.target.elements["search"].value;
+  updateImages(search);
+};
+
+// event listerers
+searchForm.addEventListener("submit", handleSearchSubmit);
+
+// render methods
+const clearPhotos = () => {
+  while (photoContainer.firstChild) {
+    photoContainer.firstChild.remove();
+  }
+};
+
+const renderPhoto = (photo, parentNode) => {
+  const template = document.importNode(imageTemplate.content, true);
+
+  const img = template.querySelector("img");
+  img.src = photo.urls.regular;
+
+  parentNode.appendChild(template);
+};
+
+const renderPhotos = grid => {
+  grid.forEach(row => {
+    const columns = document.createElement("div");
+    columns.className = "columns";
+
+    row.forEach(photo => renderPhoto(photo, columns));
+
+    photoContainer.appendChild(columns);
+  });
+};
+
+const updateImages = async search => {
+  clearPhotos();
+  try {
+    const photos = await adapter.getImages(search);
+    const grid = arrayToGrid(photos.results, 5);
+    renderPhotos(grid);
+  } catch (err) {
+    console.warn(err);
+  }
+};
+
+// initial render
+const init = () => {
+  updateImages("corgi");
+};
+
+init();
